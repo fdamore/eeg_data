9b11fe6be0d52a4f4a36108f04cc1456ae8b6700
Francesco D'Amore <francesco.damore@gmail.com>
2025-06-18 10:25:13 +0200

    Script to update SVM process and test notebook.

--- svm/svm_process.py
+++ svm/svm_process.py
@@ -8,6 +8,8 @@ import dataproc.preprocessing as dp
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 filename = '/home/francesco/git/esn_rpy/data/signal_eeg.csv'
 
@@ -30,45 +32,36 @@ print(f'Shape of the data: {X.shape}')
 print(f'Shape of the label: {y.shape}')
 print(f'Number of classes: {len(np.unique(y))}')
 
+# Create a pipeline with scaling and SVC
+pipe = Pipeline([
+    ('scaler', StandardScaler()),
+    ('svc', SVC())
+])
 
-#apply feature reduction
-data_reduced, model_ca = dp.lda_process(X, y, n_components=None)
-
-#require a specific variance threshold
-required_var = 0.95
-
-X_reduced = dp.get_data_for_variance(required_variance=required_var, ca=model_ca, X=data_reduced)
-print(f'Number of components required to reach {required_var*100}% variance: {X_reduced.shape[1]}')
-print(f'Shape of the reduced data: {X_reduced.shape}')
-
+# Update parameter grid to use pipeline step names
 params_grid = {
-    'C': [0.006, 0.015, 0.03, 0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024],
-    'gamma': ['auto', 'scale'],
-    'kernel':['rbf'],
-    'decision_function_shape': ['ovr', 'ovo'] 
-    }
+    'svc__C': [0.006, 0.015, 0.03, 0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024],
+    'svc__gamma': ['auto', 'scale'],
+    'svc__kernel': ['rbf'],
+    'svc__decision_function_shape': ['ovr', 'ovo']
+}
 
 #Create the GridSearchCV object
 nfolds = 10  # Number of cross-validation folds
-grid = GridSearchCV(SVC(), params_grid, verbose=1, cv=nfolds, n_jobs=1, scoring='accuracy')
+grid = GridSearchCV(pipe, params_grid, verbose=1, cv=nfolds, n_jobs=1, scoring='accuracy')
 
 #get time
 t_start = time.time()
 
 #Fit the data with the best possible parameters
-grid_clf = grid.fit(X_reduced, y)
+grid_clf = grid.fit(X, y)
 
 #get time training
 t_training = time.time()
 
 #Print the best estimator with it's parameters
+print(f'Number of folds (nfolds): {nfolds}')
 print(f'Best paramenter: {grid.best_params_}')
 print(f'Time training: {t_training - t_start} seconds')
 print(f'Best score {grid.best_score_}')
 print(f'Results: {grid.cv_results_.keys()}')
-
-
-
-
-
-
